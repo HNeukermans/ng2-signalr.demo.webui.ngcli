39b4d411c52d20e347598e4f276734df3cd8d021
Gittermans <hannes_neukermans@hotmail.com>
2017-06-20 19:01:50 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SignalR, SignalRConnection, IConnectionOptions } from 'ng2-signalr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private _signalR: SignalR)  {
+ }
+ngOnInit() {
+
+ }
+
+ connect() {
+    let o: IConnectionOptions;
+    let conx = this._signalR.createConnection();
+    conx.status.subscribe((s) => console.warn(s.name));
+    conx.start().then((c) => {
+
+      let listener = c.listenFor('Hello');
+      listener.subscribe(() => {
+              console.log('received hello');
+
+      });
+      c.invoke('SayHello').then(() => {
+
+      });
+    });
+  }
+
+   connect2() {
+    let o: IConnectionOptions;
+    let conx = this._signalR.connect().then((c) => {
+
+      let listener = c.listenFor('Hello');
+      listener.subscribe(() => {
+              console.log('received hello');
+
+      });
+      c.invoke('SayHello').then(() => {
+
+      });
+    });
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SignalRModule } from 'ng2-signalr';
+import { SignalRConfiguration } from 'ng2-signalr';
 
 import { AppComponent } from './app.component';
+
+export function createConfig(): SignalRConfiguration {
+  const c = new SignalRConfiguration();
+  c.hubName = 'Ng2SignalRHub';
+  c.qs = { user: 'donald' };
+  c.url = 'http://ng2-signalr-backend.azurewebsites.net/';
+  c.logging = true;
+  return c;
+}
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SignalRModule.forRoot(createConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
